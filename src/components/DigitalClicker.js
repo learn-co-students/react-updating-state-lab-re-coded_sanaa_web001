c78840db6521e3c00e8ec6f17a5f4461db29c1b1
Saleem Saleh <salem1234saleem@gmail.com>
2020-08-12 19:33:10 +0000

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,56 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+export default class DigitalClicker extends React.Component {
+
+
+   constructor() {
+    super();
+
+    this.state = {
+      timesClicked: 0,
+    };
+  }
+
+  onClick = () => {
+    this.setState(prevState => {
+        return {
+      timesClicked: prevState.timesClicked+1
+    }
+    })
+  }
+
+  render() {
+    return (
+      <button onClick={this.onClick}>{this.state.timesClicked}</button>
+    );
+  }
+}
+// Code DigitalClicker Component Here
+import React from 'react';
+
+export default class DigitalClicker extends React.Component {
+
+
+   constructor() {
+    super();
+
+    this.state = {
+      timesClicked: 0,
+    };
+  }
+
+  onClick = () => {
+    this.setState(prevState => {
+        return {
+      timesClicked: prevState.timesClicked+1
+    }
+    })
+  }
+
+  render() {
+    return (
+      <button onClick={this.onClick}>{this.state.timesClicked}</button>
+    );
+  }
+}
