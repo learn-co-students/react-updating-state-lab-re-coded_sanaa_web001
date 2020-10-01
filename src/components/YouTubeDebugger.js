f262b77331313734c4228739238689e340e28929
Hajar Helal <hajerhelal44@gmail.com>
2020-10-01 00:42:52 +0000

    Done.

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -36,19 +36,6 @@ class YouTubeDebugger extends React.Component {
         }
       }
     });
-
-    /*
-    Or this can be defined using the Object.assign method:
-
-    this.setState({
-      settings: Object.assign({}, this.state.settings, {
-        video: Object.assign({}, this.state.settings.video, {
-          resolution: '720p'
-        })
-      })
-    });
-
-    */
   };
 
   render() {
