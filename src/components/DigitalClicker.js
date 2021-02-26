0e95b046807dba00775f3afc80b7c0ae6bf2ac1e
Othman Shareef <othmanosx@gmail.com>
2021-02-26 20:08:31 +0300

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,20 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+export default class DigitalClicker extends Component {
+    constructor() {
+        super()
+        this.state = {
+            timesClicked: 0
+        }
+    }
+    render() {
+        return (
+            <div>
+                <label>times clicked:</label>
+                <button onClick={() => this.setState({ timesClicked: this.state.timesClicked + 1 })}>{this.state.timesClicked}</button>
+
+            </div>
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,40 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react'
+
+export default class YouTubeDebugger extends Component {
+    constructor() {
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        }
+    }
+    render() {
+        return (
+            <div>
+                <button className='bitrate' onClick={() => this.setState({
+                    ...this.state, settings: {
+                        bitrate: 12,
+                        video: {
+                            resolution: this.state.settings.video.resolution
+                        }
+                    }
+                })}>bitrate</button>
+                <button className='resolution' onClick={() => this.setState({
+                    ...this.state, settings: {
+                        bitrate: this.state.settings.bitrate,
+                        video: {
+                            resolution: '720p'
+                        }
+                    }
+                })}>resolution</button>
+            </div >
+        )
+    }
+}
